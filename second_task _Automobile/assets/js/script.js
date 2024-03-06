477da9d02635b9531cf9486e04e2acb8d7837ac0
shitalkatre <134849794+shitallog@users.noreply.github.com>
2024-03-06 09:01:51 +0530

    first commit

---
+++ second_task _Automobile/assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // JavaScript functionality (if needed)
+    var learnMoreButton = document.getElementById('cta-button');
+
+    learnMoreButton.addEventListener('click', function() {
+        alert("Thank you for your interest! Contact us for more details.");
+    });
+});
